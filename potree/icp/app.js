f9efad2cacceaa8b5b5141906edc9764f48844b7
Jonathan Miner <jdminer2@ncsu.edu>
2023-03-21 13:39:55 -0400

    Added free camera movement instead of orbital.
    Removed unnecessary size and position changes.

--- potree/icp/app.js
+++ potree/icp/app.js
@@ -1,10 +1,12 @@
 import {IfcAPI, IFCCARTESIANPOINT} from "web-ifc/web-ifc-api";
 import {IFCLoader} from "web-ifc-three/IFCLoader"
-import {Object3D, AmbientLight, DirectionalLight} from "THREE";
+import {Object3D, AmbientLight, DirectionalLight} from "three";
 
+let potreeFilePath = "./0611/metadata.json"
+let ifcjsFilePath = "mccormick-0416-research.ifc"
 
-// Potree
 function activate() {
+    // Potree
     window.viewer = new Potree.Viewer(document.getElementById("potree_render_area"));
 
     viewer.setEDLEnabled(true);
@@ -20,24 +22,19 @@ function activate() {
         //viewer.toggleSidebar();
     });
 
-    Potree.loadPointCloud("./0611/metadata.json", "lion", function(e){
+    Potree.loadPointCloud(potreeFilePath, "pointCloud", function(e){
         viewer.scene.addPointCloud(e.pointcloud);
         
         let material = e.pointcloud.material;
-        material.size = 1;
         material.pointSizeType = Potree.PointSizeType.ADAPTIVE;
-        
-        e.pointcloud.position.x += 3;
-        e.pointcloud.position.y -= 3;
-        e.pointcloud.position.z += 4;
-        
         viewer.fitToScreen();
-        console.log(e.pointcloud);
+        viewer.setControls(viewer.fpControls);
+
+        console.log(e);
     });
 
 
     // IFCJS
-    const ifcFileLocation = "mccormick-0416-research.ifc";
     let modelID = 0;
 
     function getIfcFile(url) {
@@ -54,8 +51,7 @@ function activate() {
 
     const ifcapi = new IfcAPI();
     ifcapi.Init().then(()=>{
-        getIfcFile(ifcFileLocation).then((ifcData) => {
-            console.log("Reached IFCJS");
+        getIfcFile(ifcjsFilePath).then((ifcData) => {
             modelID = ifcapi.OpenModel(ifcData);
             let lines = ifcapi.GetLineIDsWithType(modelID,IFCCARTESIANPOINT);
             
@@ -67,34 +63,28 @@ function activate() {
     });
 
     let ifcLoader = new IFCLoader();
-    ifcLoader.load(ifcFileLocation, (ifcModel) => {
+    ifcLoader.load(ifcjsFilePath, (ifcModel) => {
         // Add appropriately-sized ifc model
-        let scene = new Object3D();
-        viewer.scene.scene.add(scene);
+        let ifcjsContainer = new Object3D();
+        viewer.scene.scene.add(ifcjsContainer);
         
-        scene.position.z = 500;
-        scene.rotation.x = Math.PI/2;
-        scene.scale.x = 25;
-        scene.scale.y = 25;
-        scene.scale.z = 25;
+        ifcjsContainer.rotation.x = Math.PI/2;
 
-        scene.add(ifcModel);
+        ifcjsContainer.add(ifcModel);
 
 
         // Lighting
         const lightColor = 0xffffff;
 
         const ambientLight = new AmbientLight(lightColor, 0.5);
-        scene.add(ambientLight);
+        ifcjsContainer.add(ambientLight);
 
         const directionalLight = new DirectionalLight(lightColor, 1);
         directionalLight.position.set(0, 10, 0);
         directionalLight.target.position.set(-5, 0, 0);
-        scene.add(directionalLight);
-        scene.add(directionalLight.target);
+        ifcjsContainer.add(directionalLight);
+        ifcjsContainer.add(directionalLight.target);
     });
     window.removeEventListener("load",activate);
 }
-
-
 window.addEventListener("load",activate);
